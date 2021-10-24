cc177122befbf12019a94c0689ed88ff1b191303
Kris <kshahusa@gmail.com>
2021-10-24 18:39:19 -0500

    Basic gait; requires weit suspension

--- Arduino/Project-Zeta_main/Project-Zeta_main.ino
+++ Arduino/Project-Zeta_main/Project-Zeta_main.ino
@@ -3,7 +3,10 @@
 #include "functions.h"
 //#include "serial_servo.h"
 
-const int time = 3000;
+const int time = 2000;
+
+IK_out ik;
+int pos, pos1, pos2;
 
 void setup() {
   if (Debug != 1) {
@@ -26,10 +29,160 @@ void setup() {
   home(3000);
   delay(3000);
 
-  IK_out ik = IK(0, 0, 23);
-  int pos = degToPos(ik.m2, FR2);
-  int pos1 = degToPos(ik.m1, FR1);
-  int pos2 = degToPos(ik.m3, FR3);
+
+  low_set();
+
+
+  ik = IK(8, 0, 28);
+  pos = degToPos(ik.m2, FR2);
+  pos1 = degToPos(ik.m1, FR1);
+  pos2 = degToPos(ik.m3, FR3);
+  Controller.moveServo(FR2, pos, time);
+  Controller.moveServo(FR1, pos1, time);
+  Controller.moveServo(FR3, pos2, time);
+
+  pos = degToPos(ik.m2, BL2);
+  pos1 = degToPos(ik.m1, BL1);
+  pos2 = degToPos(ik.m3, BL3);
+  Controller.moveServo(BL2, pos, time);
+  Controller.moveServo(BL1, pos1, time);
+  Controller.moveServo(BL3, pos2, time);
+  delay(time);
+
+
+  ik = IK(15, 0, 30);
+  pos = degToPos(ik.m2, BL2);
+  pos1 = degToPos(ik.m1, BL1);
+  pos2 = degToPos(ik.m3, BL3);
+  Controller.moveServo(BL2, pos, time);
+  Controller.moveServo(BL1, pos1, time);
+  Controller.moveServo(BL3, pos2, time);
+
+  pos = degToPos(ik.m2, FR2);
+  pos1 = degToPos(ik.m1, FR1);
+  pos2 = degToPos(ik.m3, FR3);
+  Controller.moveServo(FR2, pos, time);
+  Controller.moveServo(FR1, pos1, time);
+  Controller.moveServo(FR3, pos2, time);
+  delay(time);
+
+
+
+  ik = IK(8, 0, 28);
+  pos = degToPos(ik.m2, FL2);
+  pos1 = degToPos(ik.m1, FL1);
+  pos2 = degToPos(ik.m3, FL3);
+  Controller.moveServo(FL2, pos, time);
+  Controller.moveServo(FL1, pos1, time);
+  Controller.moveServo(FL3, pos2, time);
+
+  pos = degToPos(ik.m2, BR2);
+  pos1 = degToPos(ik.m1, BR1);
+  pos2 = degToPos(ik.m3, BR3);
+  Controller.moveServo(BR2, pos, time);
+  Controller.moveServo(BR1, pos1, time);
+  Controller.moveServo(BR3, pos2, time);
+  delay(time);
+
+
+  ik = IK(15, 0, 30);
+  pos = degToPos(ik.m2, BR2);
+  pos1 = degToPos(ik.m1, BR1);
+  pos2 = degToPos(ik.m3, BR3);
+  Controller.moveServo(BR2, pos, time);
+  Controller.moveServo(BR1, pos1, time);
+  Controller.moveServo(BR3, pos2, time);
+
+  pos = degToPos(ik.m2, FL2);
+  pos1 = degToPos(ik.m1, FL1);
+  pos2 = degToPos(ik.m3, FL3);
+  Controller.moveServo(FL2, pos, time);
+  Controller.moveServo(FL1, pos1, time);
+  Controller.moveServo(FL3, pos2, time);
+  delay(time);
+
+  low_set();
+}
+
+void loop() {
+  low_set();
+
+  ik = IK(8, 0, 28);
+  pos = degToPos(ik.m2, FR2);
+  pos1 = degToPos(ik.m1, FR1);
+  pos2 = degToPos(ik.m3, FR3);
+  Controller.moveServo(FR2, pos, time);
+  Controller.moveServo(FR1, pos1, time);
+  Controller.moveServo(FR3, pos2, time);
+
+  pos = degToPos(ik.m2, BL2);
+  pos1 = degToPos(ik.m1, BL1);
+  pos2 = degToPos(ik.m3, BL3);
+  Controller.moveServo(BL2, pos, time);
+  Controller.moveServo(BL1, pos1, time);
+  Controller.moveServo(BL3, pos2, time);
+  delay(time);
+
+
+  ik = IK(15, 0, 30);
+  pos = degToPos(ik.m2, BL2);
+  pos1 = degToPos(ik.m1, BL1);
+  pos2 = degToPos(ik.m3, BL3);
+  Controller.moveServo(BL2, pos, time);
+  Controller.moveServo(BL1, pos1, time);
+  Controller.moveServo(BL3, pos2, time);
+
+  pos = degToPos(ik.m2, FR2);
+  pos1 = degToPos(ik.m1, FR1);
+  pos2 = degToPos(ik.m3, FR3);
+  Controller.moveServo(FR2, pos, time);
+  Controller.moveServo(FR1, pos1, time);
+  Controller.moveServo(FR3, pos2, time);
+  delay(time);
+
+
+
+  ik = IK(8, 0, 28);
+  pos = degToPos(ik.m2, FL2);
+  pos1 = degToPos(ik.m1, FL1);
+  pos2 = degToPos(ik.m3, FL3);
+  Controller.moveServo(FL2, pos, time);
+  Controller.moveServo(FL1, pos1, time);
+  Controller.moveServo(FL3, pos2, time);
+
+  pos = degToPos(ik.m2, BR2);
+  pos1 = degToPos(ik.m1, BR1);
+  pos2 = degToPos(ik.m3, BR3);
+  Controller.moveServo(BR2, pos, time);
+  Controller.moveServo(BR1, pos1, time);
+  Controller.moveServo(BR3, pos2, time);
+  delay(time);
+
+
+  ik = IK(15, 0, 30);
+  pos = degToPos(ik.m2, BR2);
+  pos1 = degToPos(ik.m1, BR1);
+  pos2 = degToPos(ik.m3, BR3);
+  Controller.moveServo(BR2, pos, time);
+  Controller.moveServo(BR1, pos1, time);
+  Controller.moveServo(BR3, pos2, time);
+
+  pos = degToPos(ik.m2, FL2);
+  pos1 = degToPos(ik.m1, FL1);
+  pos2 = degToPos(ik.m3, FL3);
+  Controller.moveServo(FL2, pos, time);
+  Controller.moveServo(FL1, pos1, time);
+  Controller.moveServo(FL3, pos2, time);
+  delay(time);
+
+  low_set();
+}
+
+void low_set() {
+  ik = IK(0, 0, 26);
+  pos = degToPos(ik.m2, FR2);
+  pos1 = degToPos(ik.m1, FR1);
+  pos2 = degToPos(ik.m3, FR3);
   Controller.moveServo(FR2, pos, time);
   Controller.moveServo(FR1, pos1, time);
   Controller.moveServo(FR3, pos2, time);
@@ -55,37 +208,4 @@ void setup() {
   Controller.moveServo(FL1, pos1, time);
   Controller.moveServo(FL3, pos2, time);
   delay(time);
-
-
-  ik = IK(15, 0, 20);
-  pos = degToPos(ik.m2, FR2);
-  pos1 = degToPos(ik.m1, FR1);
-  pos2 = degToPos(ik.m3, FR3);
-  Controller.moveServo(FR2, pos, time);
-  Controller.moveServo(FR1, pos1, time);
-  Controller.moveServo(FR3, pos2, time);
-
-
-  pos = degToPos(ik.m2, BL2);
-  pos1 = degToPos(ik.m1, BL1);
-  pos2 = degToPos(ik.m3, BL3);
-  Controller.moveServo(BL2, pos, time);
-  Controller.moveServo(BL1, pos1, time);
-  Controller.moveServo(BL3, pos2, time);
-  delay(time);
-
-  ik = IK(15, 0, 30);
-  pos = degToPos(ik.m2, BL2);
-  pos1 = degToPos(ik.m1, BL1);
-  pos2 = degToPos(ik.m3, BL3);
-  Controller.moveServo(BL2, pos, time);
-  Controller.moveServo(BL1, pos1, time);
-  Controller.moveServo(BL3, pos2, time);
-  delay(time);
-
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 }

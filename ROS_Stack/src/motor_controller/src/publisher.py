25801fa4bd5eeba3552d73f85b6bc572ec4b2e1d
Krishna Shah <kshahusa@gmail.com>
2023-02-21 10:08:53 -0600

    Fixed rate and added subtract feature

--- ROS_Stack/src/motor_controller/src/publisher.py
+++ ROS_Stack/src/motor_controller/src/publisher.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import Float64
 
 motors = [['FL_1_position_controller', 'FL_2_position_controller', 'FL_3_position_controller'], ['FR_1_position_controller', 'FR_2_position_controller', 'FR_3_position_controller'], ['BL_1_position_controller', 'BL_2_position_controller', 'BL_3_position_controller'], ['BR_1_position_controller', 'BR_2_position_controller', 'BR_3_position_controller']]
-publishers = [[None, None, None],[None, None, None],[None, None, None]]
+publishers = [[None, None, None],[None, None, None],[None, None, None], [None, None, None]]
 
 def publisher():
     for i in motors:
@@ -16,14 +16,29 @@ def publisher():
                 pass
     print(publishers)
     rospy.init_node('motor_controller_pub', anonymous=True)
-    rate = rospy.Rate(200) # 10hz
+    rate = rospy.Rate(50) # 10hz
     angle = 0
+    subtract = False
     while not rospy.is_shutdown():
         rospy.loginfo(f"Publishing...{angle}")
+        # for i in publishers:
+        #     for j in i:
+        #         j.publish(angle)
+
         publishers[0][0].publish(angle)
-        angle += 0.001
+        publishers[1][0].publish(angle)
+        publishers[2][0].publish(angle)
+        publishers[3][0].publish(angle)
+
+        if subtract:
+            angle -= 0.1
+        else:
+            angle += 0.1
+        
         if(angle >= 1):
-            angle = 0 
+            subtract = True
+        if angle <= 0:
+            subtract = False
         rate.sleep()
 
 if __name__ == '__main__':
